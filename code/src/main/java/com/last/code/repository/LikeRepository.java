f5ed7ec95f6c120c58011db292f340ca5d354f31
genebir <duarltmd1@naver.com>
2022-07-21 14:01:59 +0900

    엎고 간다잇

--- code/src/main/java/com/last/code/repository/LikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.last.code.repository;
-
-import com.last.code.model.LikeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
-}
