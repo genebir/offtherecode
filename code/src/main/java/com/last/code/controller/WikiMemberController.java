00639593120f48cc2b80f4dc062aca363032bbaf
hyeonahsong <hyeonasong@naver.com>
2022-07-26 15:50:30 +0900

    Complete wiki_member(service, controller)

--- code/src/main/java/com/last/code/controller/WikiMemberController.java
+++ code/src/main/java/com/last/code/controller/WikiMemberController.java
@@ -1,13 +1,49 @@
 package com.last.code.controller;
 
+import com.last.code.model.WikiMemberDTO;
+import com.last.code.service.WikiMemberService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("wikiMember")
 
 public class WikiMemberController {
+    @Autowired
+    private WikiMemberService service;
 
-}
+// Select all
+    @GetMapping("/list")
+    public List<WikiMemberDTO> wikiMemberList() {
+        return service.wikiMemberList();
+    } // wikiMemberList
+
+    // Select one
+    @GetMapping("/detail")
+    public WikiMemberDTO wikiMemberDetail(@RequestParam("wiki_member_pno") int wiki_member_pno) {
+        return service.wikiMemberDetail(wiki_member_pno);
+    } // wikiMemberDetail
+
+    // Insert
+    @PostMapping("/insert")
+public int wikiMemberInsert(@RequestBody WikiMemberDTO dto) {
+        return service.wikiMemberInsert(dto);
+    } // wikiMemberInsert
+
+    // Update
+    @PostMapping()
+    public int wikiMemberUpdate(@RequestBody WikiMemberDTO dto) {
+        return service.wikiMemberUpdate(dto);
+    } // wikiMemberUpdate
+
+    // Delete
+    @GetMapping("/detail")
+    public int wikiMemberDelete(@RequestParam("wiki_member_pno") int wiki_member_pno) {
+        return service.wikiMemberDelete(wiki_member_pno);
+    } // wikiMemberDelete
+
+} // class
--- code/src/main/java/com/last/code/service/WikiMemberService.java
+++ code/src/main/java/com/last/code/service/WikiMemberService.java
@@ -1,8 +1,42 @@
 package com.last.code.service;
 
+import com.last.code.model.WikiMemberDTO;
+import com.last.code.repository.WikiMemberMapperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 
 public class WikiMemberService {
-}
+    @Autowired
+    private WikiMemberMapperRepository repository;
+
+    // Select all
+    public List<WikiMemberDTO> wikiMemberList() {
+        return repository.wikiMemberList();
+    } // wikiMemberList
+
+    // Select one
+    public WikiMemberDTO wikiMemberDetail(int wiki_member_pno) {
+        return repository.wikiMemberDetail(wiki_member_pno);
+    } // wikiMemberDetail
+
+    // Insert
+    public int wikiMemberInsert(WikiMemberDTO dto) {
+        return repository.wikiMemberInsert(dto);
+    } // wikiMemberInsert
+
+    // Update
+    public int wikiMemberUpdate(WikiMemberDTO dto) {
+        return repository.wikiMemberUpdate(dto);
+    } // wikiMemberUpdate
+
+    // Delete
+    public int wikiMemberDelete(int wiki_member_pno) {
+        return repository.wikiMemberDelete(wiki_member_pno);
+    } // wikiMemberDelete
+
+
+} // class
