659215d5a1611cd4e993a0ee17ecb7b56762a794
genebir <duarltmd1@naver.com>
2022-07-30 14:02:54 +0900

    User JWT update

---
+++ code/src/main/java/com/last/code/security/WebMVCConfig.java
@@ -0,0 +1,21 @@
+package com.last.code.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
